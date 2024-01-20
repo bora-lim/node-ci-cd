221370dc8612bb8e6e002beccb92ef1b024c3e72
vivi-appknot <vivi@appknot.com>
2024-01-20 22:57:28 +0900

    [CNG] 오타 수정

--- src/app.js
+++ src/app.js
@@ -2,5 +2,5 @@ import { add } from './functions/add';
 import { sub } from './functions/sub';
 
 export const main = (a, b) => {
-    return add(a, b) + sub(a. b);
+    return add(a, b) + sub(a, b);
 };
